ee3dc0a8321a1be7495f2c37d5c04cbe2d009008
lijingjing <lijing110102@163.com>
2022-11-10 10:51:24 +0800

    全局配置sass

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,13 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/style/index.scss";`
+      }
+    }
   }
 })
+
