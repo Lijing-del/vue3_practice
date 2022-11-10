464d11236cd3013dfb55f18189fc2a5ad0574df1
Lijing-del <1053224277@qq.com>
2022-11-10 17:20:41 +0800

    根据环境变量配置代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,43 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      // 路径别名
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+type ViteProps = {
+  mode: string;
+};
+export default ({ mode }: ViteProps) => {
+  console.log(mode, 'mode')
+  console.log(loadEnv(mode, process.cwd()))
+  return defineConfig({
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        // 路径别名
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/style/index.scss";`
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/style/index.scss";`
+        }
       }
-    }
-  }
-})
-
+    },
+    server: {
+      proxy: {
+        "/api": {
+          // vite提供的loadEnv可以确认环境变量 
+          // mode可以拿到当前环境变量 development/test/production
+          // 直接读取到根目录下的配置文件 .env.dev/.env.prod/.env.test { VITE_ENV: 'test', VITE_API_URL: 'http://test/' }
+          target: loadEnv(mode, process.cwd()).VITE_API_URL,
+          ws: true,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  })
+}
